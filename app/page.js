57461f090b6bd8464dd136f3d9094f63790079b2
鬼鮫 <mmursaliev00@gmail.com>
2024-06-17 07:22:20 +0500

    - db connect (success) - added API route

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ async function getAppliances() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/testing`, getData)
   return await res.json()
 }
-
+// z
 export default async function Home() {
   const appliances = await getAppliances()
   console.log('appliances', appliances)
