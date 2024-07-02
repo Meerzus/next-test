5b3ca6c878ab8c8a6278866d1ecd971ed5a0fa55
鬼鮫 <mmursaliev00@gmail.com>
2024-07-02 22:58:17 +0500

    - db connect (failed)

--- app/api/testing/route.js
+++ app/api/testing/route.js
@@ -1,21 +1,62 @@
 import {NextResponse} from "next/server";
 import {query} from "@/lib/db";
+import mysql from "mysql2/promise";
+
+let message = ''
 
 export async function GET(){
-    const appliances = await query({
-        query: 'SELECT * FROM appliances',
+    const info = await query({
+        query: 'SELECT * FROM info',
         values: []
     })
 
-    return NextResponse.json(appliances)
+    return NextResponse.json(info)
 }
 
-export async function POST(req) {
-    const applianceType = req.body.appliance_type
-    const addAppliance = await query({
-        query: 'INSERT INTO appliances (appliance_type) VALUES (?)',
-        values: [applianceType]
+export async function POST(req, res) {
+    const {testVal} = req.body
+
+    const sanitizedData = {
+        testVal: testVal !== undefined ? testVal : null
+    };
+
+    const connection = await mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        database: process.env.MYSQL_DATABASE,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+    });
+
+    const query = 'INSERT INTO testing (testVal) VALUES (?)';
+
+    await connection.execute(query, [sanitizedData.testVal], (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        res.status(200).json({ message: 'User added successfully', results });
+    });
+
+    // const addTest = await query({
+    //     // query: 'INSERT INTO info (city) VALUES (?)',
+    //     query: 'INSERT INTO testing (testVal) VALUES (?)',
+    //     values: [testVal !== undefined ? testVal : null]
+    // })
+    //
+    const info = await query({
+        query: 'SELECT * FROM testing',
+        values: []
     })
 
-    return NextResponse.json(addAppliance)
+    // if (addTest.insertId) {
+    //     message = "Success"
+    // } else message = "Error"
+
+    // let test = {
+    //     id: addTest.insertId,
+    //     testVal: testBody
+    // }
+
+    return NextResponse.json(info)
 }
